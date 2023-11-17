fe9234bd7b9b6a81e68349a3bfb1be2849d2080c
Abhay Ramesh <ramesh.abhay14@gmail.com>
2023-11-17 12:29:43 +0530

    fix: Bugfix of Icon load more and searching feat Fix: Load more Scrolling #3

--- components/icons/LucideIconStatic.tsx
+++ components/icons/LucideIconStatic.tsx
@@ -3,7 +3,7 @@ import { LucideProps } from "lucide-react";
 import { IconProps } from ".";
 
 export const LucideIconStatic = ({ name, ...props }: IconProps) => {
-  // Convert to CapitalCase with no dashes with all first letters capitalized and allow numbers too
+  // Convert to CapitalCase with no dashes with all first letters capitalized and allow numbers too.
   const newName = name
     .replace(/-([a-z])/g, (g) => g[1].toUpperCase())
     .replace(/^[a-z]/g, (g) => g.toUpperCase())
