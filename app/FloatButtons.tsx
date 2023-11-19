b64c604368c71b39b81eaa559bbb20a0124c62be
Abhay Ramesh <ramesh.abhay14@gmail.com>
2023-11-19 23:58:21 +0530

    Posthog

--- app/FloatButtons.tsx
+++ app/FloatButtons.tsx
@@ -7,12 +7,12 @@ export function FloatButtons() {
   return (
     <div className="fixed bottom-5 right-5 z-20 flex flex-col items-end justify-end space-y-2">
       <DownloadButton />
-      <Link href="/feedback">
-        <Button variant="outline" className="h-fit w-fit rounded-full p-4">
+      {/* <Link href="/feedback">
+        <Button variant="outline" className="p-4 rounded-full h-fit w-fit">
           <ScrollText size={24} className="mr-2" />
           Feedback
         </Button>
-      </Link>
+      </Link> */}
     </div>
   );
 }
--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,13 +1,23 @@
 "use client";
 import { ThemeProvider } from "@/components/custom/theme-provider";
 import { Provider as JotaiProvider } from "jotai";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  });
+}
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <JotaiProvider>
-      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-        {children}
-      </ThemeProvider>
+      <PostHogProvider client={posthog}>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+          {children}
+        </ThemeProvider>
+      </PostHogProvider>
     </JotaiProvider>
   );
 }
