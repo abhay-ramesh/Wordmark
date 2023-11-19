dc9698d9b7f7d5104689815af80a876aecd6a07e
Abhay Ramesh <ramesh.abhay14@gmail.com>
2023-11-20 00:34:58 +0530

    Posthog

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -4,20 +4,28 @@ import { Provider as JotaiProvider } from "jotai";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
-if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  });
+if (process.env.NEXT_PUBLIC_ENABLE_POSTHOG === "true") {
+  if (typeof window !== "undefined") {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    });
+  }
 }
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <JotaiProvider>
-      <PostHogProvider client={posthog}>
+      {process.env.NEXT_PUBLIC_ENABLE_POSTHOG === "true" ? (
+        <PostHogProvider client={posthog}>
+          <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+            {children}
+          </ThemeProvider>
+        </PostHogProvider>
+      ) : (
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
         </ThemeProvider>
-      </PostHogProvider>
+      )}
     </JotaiProvider>
   );
 }
