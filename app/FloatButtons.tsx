7afd68a46490165524b406e5af854e5175584fbc
Abhay Ramesh <ramesh.abhay14@gmail.com>
2023-11-20 14:45:41 +0530

    Fix: DarkModeTaggle

--- app/FloatButtons.tsx
+++ app/FloatButtons.tsx
@@ -1,7 +1,4 @@
-import { Button } from "@/components/ui/button";
 import { DownloadButton } from "./DownloadButton";
-import Link from "next/link";
-import { ScrollText } from "lucide-react";
 import { ModeToggle } from "@/components/ui/theme-button";
 
 export function FloatButtons() {
@@ -9,12 +6,6 @@ export function FloatButtons() {
     <div className="fixed bottom-5 right-5 z-20 flex flex-col items-center justify-end space-y-2">
       <ModeToggle />
       <DownloadButton />
-      {/* <Link href="/feedback">
-        <Button variant="outline" className="p-4 rounded-full h-fit w-fit">
-          <ScrollText size={24} className="mr-2" />
-          Feedback
-        </Button>
-      </Link> */}
     </div>
   );
 }
