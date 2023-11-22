aa09d07b49d3d07d83e22496da07f38d557a3477
Abhay Ramesh <ramesh.abhay14@gmail.com>
2023-11-22 09:54:30 +0530

    Star on Github

--- app/Credits.tsx
+++ app/Credits.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import Script from "next/script";
 
 export function Credits({
   className,
@@ -20,12 +21,28 @@ export function Credits({
         className,
       )}
     >
-      <Link
+      <div className="mx-auto flex h-fit w-fit items-center gap-2 rounded-md border p-2 text-center align-middle">
+        <div className="font-semibold">Star Us on Github</div>
+        <Script
+          src="https://buttons.github.io/buttons.js"
+          strategy="afterInteractive"
+        />
+        <Link
+          className="github-button ml-2"
+          href="https://github.com/abhay-ramesh/Wordmark"
+          data-color-scheme="no-preference: light; light: light; dark: dark;"
+          data-show-count="true"
+          aria-label="Star abhay-ramesh/Wordmark on GitHub"
+        >
+          Star
+        </Link>
+      </div>
+      {/* <Link
         href="https://www.producthunt.com/posts/wordmark-7584b39f-f3da-41d9-aedf-18c59cf1228e?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-wordmark&#0045;7584b39f&#0045;f3da&#0045;41d9&#0045;aedf&#0045;18c59cf1228e"
         target="_blank"
-        className="flex w-full justify-center"
+        className="flex justify-center w-full"
       >
-        {/* eslint-disable-next-line @next/next/no-img-element */}
+
         <img
           src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=425883&theme=light"
           alt="Wordmark&#0046; - Google&#0032;font&#0032;logo&#0032;maker | Product Hunt"
@@ -33,7 +50,7 @@ export function Credits({
           width="250"
           height="54"
         />
-      </Link>
+      </Link> */}
       <div>
         Created by{" "}
         <Link
